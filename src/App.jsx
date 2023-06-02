ce9e72803b50b1bdba750bd67944e8853773d618
unknown <ruslan.horaiskyi@gmail.com>
2023-06-02 15:35:01 +0300

    Add close button for MoreInfo at the end of Cards list

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import { useCallback, useState } from 'react';
 import 'modern-normalize';
-
-import MoreInfo from './components/MoreInfo/MoreInfo';
 import CardList from './components/CardList/CardList';
-
 import './App.css';
 
 const App = () => {
   const [focusedCard, setFocusedCard] = useState(null);
-  console.log('App');
 
-  const handleCardFocus = useCallback(
-    (date) => {
-      setFocusedCard(date);
-    },
-    [setFocusedCard]
-  );
+  const handleCardFocus = useCallback((date) => {
+    setFocusedCard(date);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setFocusedCard(null);
+  }, []);
 
   return (
-    <>
-      <div className='container'>
-        <CardList handleCardFocus={handleCardFocus} focusedCard={focusedCard} />
-      </div>
-      {focusedCard && <MoreInfo data={focusedCard} />}
-    </>
+    <div className='container'>
+      <CardList
+        handleCardFocus={handleCardFocus}
+        focusedCard={focusedCard}
+        onClose={handleClose}
+      />
+    </div>
   );
 };
 
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,30 +1,42 @@
 import PropTypes from 'prop-types';
-
 import weatherData from '../../constants/data.json';
 import Card from './Card/Card';
+import MoreInfo from '../MoreInfo/MoreInfo';
+import './CardList.css';
 
-const CardList = ({ focusedCard, handleCardFocus }) => {
-  console.log('CardList');
-
-  return !weatherData ? (
-    <div>No weather data</div>
-  ) : (
-    weatherData.map((dayData) => (
-      <Card
-        key={dayData.date}
-        isActive={focusedCard?.date === dayData.date}
-        data={dayData}
-        handleClick={handleCardFocus}
-      />
-    ))
-  );
-};
+const CardList = ({ focusedCard, handleCardFocus, onClose }) => (
+  <div className='cardListContainer'>
+    <div className='cardList'>
+      {!weatherData ? (
+        <div>No weather data</div>
+      ) : (
+        weatherData.map((dayData) => (
+          <Card
+            key={dayData.date}
+            isActive={focusedCard?.date === dayData.date}
+            data={dayData}
+            handleClick={handleCardFocus}
+          />
+        ))
+      )}
+      <button type='button' className='closeButton' onClick={onClose}>
+        X
+      </button>
+    </div>
+    {focusedCard && (
+      <div className='moreInfoContainer'>
+        <MoreInfo data={focusedCard} />
+      </div>
+    )}
+  </div>
+);
 
 CardList.propTypes = {
   focusedCard: PropTypes.shape({
     date: PropTypes.string.isRequired,
   }),
   handleCardFocus: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 CardList.defaultProps = {
