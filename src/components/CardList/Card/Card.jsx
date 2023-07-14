f5523d952449286be5cfbaf6710e772cb3035aea
unknown <ruslan.horaiskyi@gmail.com>
2023-07-14 11:29:11 +0300

    Refactoring

--- src/components/CardList/Card/Card.jsx
+++ src/components/CardList/Card/Card.jsx
@@ -1,11 +1,11 @@
 import { useCallback, memo } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-
 import { roundValue } from '../../../utils/utils';
-
 import CustomIcon from '../../CustomIcon/CustomIcon';
 import styles from './Card.module.css';
+
+const celsiusIcon = '℃';
 
 const Card = memo(({ data, isActive, handleClick }) => {
   const clickHandler = useCallback(
@@ -16,13 +16,14 @@ const Card = memo(({ data, isActive, handleClick }) => {
     [handleClick, data]
   );
 
-  const { currentDay = '', weather = [], main = {} } = data ?? {};
+  const { currentDay = '', weather = [], main = {} } = data || {};
   const date = new Date(currentDay);
   const month = date.toLocaleString('en-US', { month: 'long' });
   const day = date.getDate();
   const weekday = date.toLocaleString('en-US', { weekday: 'long' });
 
-  const celsiusIcon = '℃';
+  const minTemp = roundValue(main.temp_min, celsiusIcon);
+  const maxTemp = roundValue(main.temp_max, celsiusIcon);
 
   return (
     <div
@@ -36,14 +37,14 @@ const Card = memo(({ data, isActive, handleClick }) => {
       <div className={styles.day}>{day}</div>
       <div className={styles.month}>{month}</div>
       <div className={styles.cardImage}>
-        <CustomIcon weatherStatus={weather[0].main} />
+        <CustomIcon weatherStatus={weather[0]?.main} />
       </div>
       <div className={styles.temperature}>
         <div>
-          min. <span>{roundValue(main.temp_min, celsiusIcon)} </span>
+          min. <span>{minTemp}</span>
         </div>
         <div>
-          max. <span>{roundValue(main.temp_max, celsiusIcon)} </span>
+          max. <span>{maxTemp}</span>
         </div>
       </div>
     </div>
@@ -52,19 +53,27 @@ const Card = memo(({ data, isActive, handleClick }) => {
 
 Card.propTypes = {
   data: PropTypes.shape({
-    currentDay: PropTypes.string.isRequired,
+    currentDay: PropTypes.string,
     weather: PropTypes.arrayOf(
       PropTypes.shape({
         main: PropTypes.string.isRequired,
       })
-    ).isRequired,
+    ),
     main: PropTypes.shape({
       temp_min: PropTypes.number.isRequired,
       temp_max: PropTypes.number.isRequired,
-    }).isRequired,
-  }).isRequired,
+    }),
+  }),
   isActive: PropTypes.bool.isRequired,
   handleClick: PropTypes.func.isRequired,
+};
+
+Card.defaultProps = {
+  data: {
+    currentDay: '',
+    weather: [],
+    main: {},
+  },
 };
 
 export default Card;
